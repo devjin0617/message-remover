564f5c375a536ab20993d4c3b5ae51c91bf2a1c9
jin-mealant <jin@mealant.com>
2019-10-28 18:30:04 +0900

    Add token form

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,16 +1,42 @@
 <template>
   <div id="app">
+    <div class="container">
+      <el-input style="width:400px;" placeholder="input slack legacy token"></el-input>
+      <div style="margin-top:10px;">
+        <el-button type="primary" :round="true" @click="saveToken">save token</el-button>
+      </div>
+      {{ message }}
+    </div>
     <!-- <router-view></router-view> -->
-    Hello World
   </div>
 </template>
 
 <script>
   export default {
-    name: 'slack-message-remover'
+    name: 'slack-message-remover',
+    data () {
+      return {
+        message: ''
+      }
+    },
+    methods: {
+      saveToken () {
+        this.message = 'saved'
+      }
+    }
   }
 </script>
 
 <style>
 /* CSS */
+#app {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.container {
+  text-align: center;
+}
 </style>
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,13 +5,20 @@ import App from './App'
 import router from './router'
 import store from './store'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
+  components: {
+    App
+  },
   router,
   store,
   template: '<App/>'
