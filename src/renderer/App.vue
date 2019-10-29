5fe35d83c9918bd44a806382aed9d425f7979294
jin-mealant <jin@mealant.com>
2019-10-29 14:48:39 +0900

    Add get slack channels

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,30 +1,72 @@
 <template>
   <div id="app">
     <div class="container">
-      <el-input style="width:400px;" placeholder="input slack legacy token"></el-input>
-      <div style="margin-top:10px;">
-        <el-button type="primary" :round="true" @click="saveToken">save token</el-button>
+      <div>
+        <h1>Message Remover</h1>
       </div>
-      {{ message }}
+      <div style="margin-top:20px;">
+        <el-input style="width:400px;" placeholder="input slack legacy token" v-model="token"></el-input>
+        <div style="margin-top:10px;">
+          <el-button type="primary" :round="true" @click="saveToken">save token</el-button>
+          <el-button type="primary" :round="true" @click="getChannels">get channels</el-button>
+        </div>
+        {{ message }}
+        <div v-if="channels.length > 0" style="margin-top:20px;">
+          <el-select v-model="value" placeholder="select channel" @change="selectChannel">
+            <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item"></el-option>
+          </el-select>
+        </div>
+      </div>
     </div>
     <!-- <router-view></router-view> -->
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'slack-message-remover',
-    data () {
-      return {
-        message: ''
-      }
+import axios from 'axios'
+axios.defaults.baseURL = 'https://slack.com/api'
+
+export default {
+  name: 'slack-message-remover',
+  data () {
+    return {
+      token: '',
+      message: '',
+      value: null,
+      channels: []
+    }
+  },
+  mounted () {
+    const localToken = localStorage('token')
+    if (localToken) {
+      this.token = localToken
+    }
+  },
+  methods: {
+    saveToken () {
+      localStorage.setItem('token', this.token)
+      this.$message({
+        message: '토큰이 저장되었습니다.',
+        type: 'success'
+      })
     },
-    methods: {
-      saveToken () {
-        this.message = 'saved'
-      }
+    async getChannels () {
+      const { data } = await axios.get('/channels.list', {
+        params: {
+          token: this.token
+        }
+      })
+      this.$message({
+        message: '채널목록을 불러왔습니다.',
+        type: 'success'
+      })
+      this.channels = data.channels
+    },
+    selectChannel ($event) {
+      this.message = JSON.stringify($event)
     }
   }
+}
 </script>
 
 <style>
