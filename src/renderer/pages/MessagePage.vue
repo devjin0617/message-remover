2ad023d1014041a0934dd6ed2a1158778cd25507
developerjin <devjin0617@gmail.com>
2019-11-01 23:55:32 +0900

    Update el table

--- src/renderer/pages/MessagePage.vue
+++ src/renderer/pages/MessagePage.vue
@@ -2,12 +2,17 @@
   <div>
     <h2>Message Page</h2>
     <div>
-      <ul>
+      <el-table :data="list" style="width: 100%">
+        <el-table-column prop="user" label="user" width="180"></el-table-column>
+        <el-table-column prop="text" label="text"></el-table-column>
+        <el-table-column prop="ts" label="ts"></el-table-column>
+      </el-table>
+      <!-- <ul>
         <li
           v-for="(item, index) in list"
           :key="index"
         >{{ item.user }}: {{ item.text }} ({{ item.ts }})</li>
-      </ul>
+      </ul>-->
     </div>
   </div>
 </template>
